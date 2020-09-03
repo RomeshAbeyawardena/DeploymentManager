bbf263614196fda07ed0246b5b4d928e740c466b
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2020-09-03 22:26:53 +0100

    Added console logging

--- src/DeploymentManager.Applet/Program.cs
+++ src/DeploymentManager.Applet/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 using DeploymentManager.Services;
 using DNI.Core.Contracts.Builders;
@@ -6,7 +7,8 @@ using DNI.Core.Services.Builders;
 using DNI.Core.Services.Extensions;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
 namespace DeploymentManager.Applet
 {
     public static class Program
@@ -15,8 +17,9 @@ namespace DeploymentManager.Applet
         {
             await BuildAppHost.ConfigureAppHost(appHost => appHost
                 .ConfigureServices(ConfigureServices))
+                .ConfigureLogging(logging => logging.AddConsole())
                 .Build<Startup>()
-                .UseStartup(async(startup) => await startup.RunAsync())
+                .UseStartup(async(startup) => await startup.RunAsync(CancellationToken.None), async(startup, cancellationToken) => await startup.StopAsync(cancellationToken))
                 .RunAsync();
         }
 
